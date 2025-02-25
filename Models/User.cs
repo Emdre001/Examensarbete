2f58e915c992b342cb4c55527442e9eeb8d0cfa4
Emdre001 <emil.dressel@gmail.com>
2025-02-25 11:04:40 +0100

    User Model Added

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace Models;
+
+public class User
+{
+    public GUID UserID { get; set; }
+    public string UserName { get; set; }
+    public string UserEmail { get; set; }
+    public string UserPassword { get; set; }
+    public string UserAddress { get; set; }
+    public int UserPhoneNr { get; set; }
+    public string UserRole { get; set; }
+}
