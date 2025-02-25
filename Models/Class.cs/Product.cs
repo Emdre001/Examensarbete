308de30b36b15c3556de3473170df354583942b7
Emiilijaaa <emmafilip17@gmail.com>
2025-02-25 11:12:46 +0100

    Update models Product.cs

--- Models/Class.cs/Product.cs
+++ Models/Class.cs/Product.cs
@@ -1,1 +1,14 @@
-//Emilija Test
+using System.Collections.Generic;
+
+namespace Models;
+
+public class Product
+{
+    public GUID ProductId { get; set; }
+    public string ProductName { get; set; }
+    public string ProductType { get; set; }
+    public string ProductDescription { get; set; }
+    public int ProductStock { get; set; }
+    public int ProductPrice { get; set; }
+    public int ProductRating { get; set; }
+}
