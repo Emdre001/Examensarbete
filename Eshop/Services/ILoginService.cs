3854c412e739dabe093755966c0707fd95436ed5
Robinracho <robin.racho@edu.newton.se>
2025-02-26 12:22:12 +0100

    Added Services

---
+++ Eshop/Services/ILoginService.cs
@@ -0,0 +1,9 @@
+using System;
+using Models.DTO;
+ 
+namespace Services;
+ 
+public interface ILoginService
+{
+    public Task<ResponseItemDto<LoginUserSessionDto>> LoginUserAsync(LoginCredentialsDto usrCreds);
+}
