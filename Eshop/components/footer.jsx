0de324b4f8c309678b920ce554e607bc6d2979e9
Emiilijaaa <emmafilip17@gmail.com>
2025-02-28 12:03:11 +0100

    Påbörjat frontend, skapat jsx, css & components

---
+++ Eshop/components/footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+    return (
+        <div className="container">
+            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <div className="col-md-4 d-flex align-items-center">
+                    <span className="mb-3 mb-md-0 text-body-secondary">© 2025 E-Shop</span>
+                </div>
+                <div className="col-md-4 d-flex justify-content-end align-items-center">
+                    <span className="mb-3 mb-md-0 text-body-secondary">(c) E-Shop Team</span>
+                </div>
+            </footer>
+        </div>
+    );
+}
