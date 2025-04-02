a65ca38ffd1735293a1564b55cef3f1c56190547
Emiilijaaa <emmafilip17@gmail.com>
2025-04-02 17:51:51 +0200

    Frontend start

--- Eshop/components/footer.jsx
+++ Eshop/components/footer.jsx
@@ -3,10 +3,10 @@ export default function Footer() {
         <div className="container">
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
                 <div className="col-md-4 d-flex align-items-center">
-                    <span className="mb-3 mb-md-0 text-body-secondary">© 2025 E-Shop</span>
+                    <span className="mb-3 mb-md-0 text-body-secondary">© 2025 ShoeStore</span>
                 </div>
                 <div className="col-md-4 d-flex justify-content-end align-items-center">
-                    <span className="mb-3 mb-md-0 text-body-secondary">(c) E-Shop Team</span>
+                    <span className="mb-3 mb-md-0 text-body-secondary">(c) ShoeStore Team</span>
                 </div>
             </footer>
         </div>
