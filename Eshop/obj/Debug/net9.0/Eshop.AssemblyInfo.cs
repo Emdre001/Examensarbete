bda8c489d5cd8283eee73254a755e60554b5c070
Emdre001 <emil.dressel@gmail.com>
2025-05-14 16:39:46 +0200

    Test

--- Eshop/obj/Debug/net9.0/Eshop.AssemblyInfo.cs
+++ Eshop/obj/Debug/net9.0/Eshop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eshop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbd8bd047dcc5e97633511d8192dd671dff321b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39ec527e79f80d4cc2a9f41d3d6c1704a454d7fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eshop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eshop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
