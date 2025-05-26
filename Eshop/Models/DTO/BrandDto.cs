9aeea7cbde9921522db4f1a0924cdad1e8e7474d
Eddie <143506824+TikkaKiwi@users.noreply.github.com>
2025-05-26 17:51:40 +0200

    Clear commit

--- Eshop/Models/DTO/BrandDto.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Models.DTO;
-
-public class BrandDTO
-{
-    public string BrandName { get; set; }
-  
-}
--- Eshop/Models/DTO/SizeDto.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Models.DTO;
-
-public class SizeDTO
-{
-    public int SizeValue { get; set; }
-    
-}
